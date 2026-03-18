0dcbfe0596332db8d4686e1a5f3683d3d64b7fd2
Abhishek Verma <abhishekjbverma.com>
2026-03-18 18:04:37 +0530

    Updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://jobpilot-backend-005n.onrender.com/api"
 });
 
 API.interceptors.request.use((req)=>{
